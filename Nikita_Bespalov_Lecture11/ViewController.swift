3db2127597fa479d4243b0662bc34c586271a4cd
Nikita <n.bespalov.2001@gmail.com>
2023-05-24 00:03:12 +0400

    Initial Commit

---
+++ Nikita_Bespalov_Lecture11/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Nikita_Bespalov_Lecture11
+//
+//  Created by Nikita on 2023-05-24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
